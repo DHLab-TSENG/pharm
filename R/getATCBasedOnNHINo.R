f0c276f7a508a96c82503c6b75c527f05b900861
lawrance39 <lawrance39@gmail.com>
2018-10-19 15:37:50 +0800

    update document

--- R/getATCBasedOnNHINo.R
+++ R/getATCBasedOnNHINo.R
@@ -3,11 +3,7 @@
 #' @import dplyr
 #' @param df data.frame include NHINO
 #' @param NHINoColName A colum for NHINo of df
-#' @param Differ_ColName if colum for NHINo of df not named "NHINo"
 #' @export
-#' @example
-#' NHINoToAtc(NHINoList)
-#'
 
 get.AtcViaNHINo <- function(df, NHINoColName = NHINo){
 
