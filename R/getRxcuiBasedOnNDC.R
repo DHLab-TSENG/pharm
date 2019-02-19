56f3ec94d654578179872109d71e3811dbbe22cb
lawrance39 <lawrance39@gmail.com>
2019-02-19 15:56:33 +0800

    support retired ndc code

--- R/getRxcuiBasedOnNDC.R
+++ R/getRxcuiBasedOnNDC.R
@@ -3,8 +3,7 @@
 #' @import doParallel
 #' @import dplyr
 #' @param df data.frame include NDC
-#' @param AtcColName A colum for NDC of df
-#' @param Differ_ColName if colum for NHINo of df not named "NDC"
+#' @param NdcColName A colum for NDC of df
 #' @param cores number of parallel operation
 #' @export
 
@@ -19,20 +18,20 @@ get.RxCuiViaNdc <- function(df, NdcColName = NDC, cores=8){
                          .combine = "rbind",
                          .packages = "httr") %dopar% {
 
-                           JSON <- GET(paste0("https://rxnav.nlm.nih.gov/REST/rxcui.json?idtype=NDC&id=", dfu$NDC[i]), timeout(60))
+                           JSON <- GET(paste0("https://rxnav.nlm.nih.gov/REST/ndcstatus?ndc=", dfu$NDC[i]), timeout(60))
                            if(http_error(JSON)){
                              rxTable <- data.frame(NDC = dfu$NDC[i],
                                                    RxCui = "error",
                                                    stringsAsFactors = FALSE)
                            }else{
                            rxid <- content(JSON)
-                           if(is.null(rxid$idGroup$rxnormId[[1]])){
+                           if(is.null(rxid$ndcStatus$rxcui)){
                              rxTable <- data.frame(NDC = dfu$NDC[i],
                                                    RxCui = NA,
                                                    stringsAsFactors = FALSE)
                            }else{
                              rxTable <- data.frame(NDC = dfu$NDC[i],
-                                                   RxCui = rxid$idGroup$rxnormId[[1]],
+                                                   RxCui = rxid$ndcStatus$rxcui,
                                                    stringsAsFactors = FALSE)
                            }
                            rxTable
